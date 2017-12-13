4f61a87e545843b7e79810370d59cbdceb110874
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-13 14:26:28 -0500

    Update show_account.php

--- pages/show_account.php
+++ pages/show_account.php
@@ -16,38 +16,31 @@
 </head>
 
 <body>
+<?php include("logout.php");?>
 
-<h1>Email: <?php echo $data->email; ?></h1>
-<h1>First Name: <?php echo $data->fname; ?></h1>
-<h1>Last Name: <?php echo $data->lname; ?></h1>
-
-
-<?php
-//this is how you print something  $data contains the record that was selected on the table.
-
-print_r($data);
-
-
-?>
-
+<h1>MY PROFILE</h1>
 <form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
-
-    First name: <input type="text" name="fname" value="<?php echo $data->fname; ?>"><br>
-
-    Last name: <input type="text" name="lname" value="<?php echo $data->lname; ?>"><br>
-    Email: <input type="text" name="email" value="<?php echo $data->email; ?>"><br>
-    Phone: <input type="text" name="phone" value="<?php echo $data->phone; ?>"><br>
-    Birthday: <input type="text" name="birthday" value="<?php echo $data->birthday; ?>"><br>
-    Gender: <input type="text" name="gender" value="<?php echo $data->gender; ?>"><br>
-    <input type="submit" value="Submit form">
+    <div class="login-page">
+        <div class="form">
+            <form class="register-form">
+                <input type="text" placeholder="First name" name="fname" value="<?php echo $data->fname; ?>"><br>
+                <input type="text" placeholder="Last name" name="lname" value="<?php echo $data->lname; ?>"><br>
+                <input type="text" placeholder="Email" name="email" value="<?php echo $data->email; ?>"><br>
+                <input type="text" placeholder="Phone" name="phone" value="<?php echo $data->phone; ?>"><br>
+                <input type="date" placeholder="Birthday" name="birthday" value="<?php echo $data->birthday; ?>"><br>
+                <input type="text" placeholder="Gender" name="gender" value="<?php echo $data->gender; ?>"><br>
+                <button type="submit" value="Submit form">UPDATE</button>
+            </form>
+        </div>
+    </div>
 </form>
 
-
-<form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+<!--
+<form action="index.php?page=accounts&action=delete&id=<?php /*echo $data->id; */?> " method="post" id="form1">
     <button type="submit" form="form1" value="delete">Delete</button>
 </form>
 
-
+-->
 <script src="js/scripts.js"></script>
 </body>
 </html>
