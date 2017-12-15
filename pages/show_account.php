5e7b445d953616f071ea6f076dcaba988bdc14fd
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-15 18:51:40 -0500

    Update show_account.php

--- pages/show_account.php
+++ pages/show_account.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Final Project</title>
+    <meta name="description" content="Final Project">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -15,32 +15,26 @@
     <![endif]-->
 </head>
 
-<body>
-<?php include("logout.php");?>
 
-<h1>MY PROFILE</h1>
-<form action="index.php?page=accounts&action=save&id=<?php echo $data->id; ?>" method="post">
+<body>
+<?php include("header.php");?>
+<div style="margin-left:25%;padding:1px 16px;">
     <div class="login-page">
-        <div class="form">
-            <form class="register-form">
-                <input type="text" placeholder="First name" name="fname" value="<?php echo $data->fname; ?>"><br>
-                <input type="text" placeholder="Last name" name="lname" value="<?php echo $data->lname; ?>"><br>
-                <input type="text" placeholder="Email" name="email" value="<?php echo $data->email; ?>"><br>
-                <input type="text" placeholder="Phone" name="phone" value="<?php echo $data->phone; ?>"><br>
-                <input type="date" placeholder="Birthday" name="birthday" value="<?php echo $data->birthday; ?>"><br>
-                <input type="text" placeholder="Gender" name="gender" value="<?php echo $data->gender; ?>"><br>
-                <button type="submit" value="Submit form">UPDATE</button>
+        <div class="form" style="margin-top: inherit; height: inherit; margin-bottom: inherit; ">
+            <h1>MY PROFILE</h1>
+            <p><h4>Email: <?php echo $data->email; ?></h4>
+            <h4>First Name: <?php echo $data->fname; ?></h4>
+            <h4>Last Name: <?php echo $data->lname; ?></h4>
+            <h4>Phone: <?php echo $data->phone; ?></h4>
+            <h4>Birthday: <?php echo $data->birthday; ?></h4>
+            <h4>Gender: <?php echo $data->gender; ?></h4></p><br>
+            <input style="width: 150px; font-family: Roboto, sans-serif; text-transform: uppercase; outline: 0; background: #4CAF50;  border: 0; padding: 15px;
+            color: #FFFFFF; font-size: 14px; cursor: pointer; position: relative" type="button" value="EDIT" onclick="window.location.href='index.php?page=accounts&action=edit'" />
+            <form action="index.php?page=accounts&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+                <button type="submit" form="form1" value="delete" style="width: 150px; position: relative">Delete</button>
             </form>
         </div>
     </div>
-</form>
-
-<!--
-<form action="index.php?page=accounts&action=delete&id=<?php /*echo $data->id; */?> " method="post" id="form1">
-    <button type="submit" form="form1" value="delete">Delete</button>
-</form>
-
--->
-<script src="js/scripts.js"></script>
+</div>
 </body>
 </html>
