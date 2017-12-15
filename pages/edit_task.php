b04ac9c27366d6eebfb1961de8bf30938a93fb3a
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-15 18:49:31 -0500

    Update edit_task.php

--- pages/edit_task.php
+++ pages/edit_task.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Final Project</title>
+    <meta name="description" content="Final Project">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -14,10 +14,9 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
 </head>
-
+<?php include("header.php");?>
 <body>
-<?php include("logout.php");?>
-
+<div style="margin-left:25%;padding:1px 16px;height:100px;">
 <form action="index.php?page=tasks&action=save&id=<?php echo $data->id; ?>" method="post">
     <div class="login-page">
         <div class="form">
@@ -26,15 +25,15 @@
                 <input type="datetime" placeholder="Due Date" name="duedate" value="<?php echo $data->duedate; ?>"><br>
                 <input type="text" placeholder="Message" name="message" value="<?php echo $data->message; ?>"><br>
                 <input type="text" placeholder="Is Done" name="isdone" value="<?php echo $data->isdone; ?>"><br>
-                <button type="submit" value="Submit form" style="width: 150px; position: relative">UPDATE</button>
-                <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
-                    <button type="submit" form="form1" value="delete" style="width: 150px; position: relative">Delete</button>
-                </form>
+                <button type="submit" value="Submit form" style="width: 150px; position: relative">UPDATE</button><br><br>
+            </form>
+            <form action="index.php?page=tasks&action=delete&id=<?php echo $data->id; ?> " method="post" id="form1">
+                <button type="submit" form="form1" value="delete" style="width: 150px; position: relative">Delete</button>
             </form>
         </div>
     </div>
 </form>
-
+</div>
 
 </body>
 </html>
