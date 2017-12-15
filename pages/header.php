0558d66c8714b3758ce7c1b813f00907e4508bab
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-15 18:50:34 -0500

    Update and rename logout.php to header.php

---
+++ pages/header.php
@@ -0,0 +1,27 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+
+    <title>Final Project</title>
+    <meta name="description" content="Final Project">
+    <meta name="author" content="SitePoint">
+
+    <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
+
+</head>
+<body>
+<ul>
+    <li><a href="index.php?page=accounts&action=dashboard">Homepage</a></li>
+    <li><a href="index.php?page=accounts&action=show">My Profile</a></li>
+    <li><a href="index.php?page=tasks&action=create">Create Task</a></li>
+    <li><a href="index.php?page=tasks&action=all">Show All Tasks</a></li>
+    <li><a href="index.php?">Logout</a></li>
+</ul>
+</body>
+</html>
