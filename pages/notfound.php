98c70a7a303141958b63f04b5d7724d108f38b4a
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-15 18:51:07 -0500

    Update notfound.php

--- pages/notfound.php
+++ pages/notfound.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
-    <meta name="description" content="The HTML5 Herald">
+    <title>Final Project</title>
+    <meta name="description" content="Final Project">
     <meta name="author" content="SitePoint">
 
     <link rel="stylesheet" href="css/styles.css?v=1.0">
@@ -14,12 +14,12 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
     <![endif]-->
 </head>
-
+<?php include("header.php");?>
 <body>
-<?php include("logout.php");?>
-<h1>Page Not Found 404</h1>
 
+<div style="margin-left:25%;padding:1px 16px;height:100px;">
+    <h1>Page Not Found 404</h1>
 
-<script src="js/scripts.js"></script>
+</div>
 </body>
 </html>
