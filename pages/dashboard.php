488919d0e0c16869d6e51d637a3c7358066932a8
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-15 18:49:00 -0500

    Update dashboard.php

--- pages/dashboard.php
+++ pages/dashboard.php
@@ -3,17 +3,26 @@
 <html lang="en">
 <head>
     <meta charset="utf-8">
+
     <title>Final Project</title>
-    <meta name="description" content="Web Systems Development IS601 Fall 2017 Project">
-    <meta name="author" content="TanukaNayak">
+    <meta name="description" content="Final Project">
+    <meta name="author" content="SitePoint">
+
     <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+    <!--[if lt IE 9]>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.js"></script>
+    <![endif]-->
 </head>
+
 <body>
 
-<?php include("logout.php");?>
-<div class="body">
-    <h1>WELCOME TO THE ACTIVE RECORD</h1>
-</div>
+<?php include("header.php");
+$headerClass='dashboard';
+$header='dashboard';?>
+<div style="margin-left:25%;padding:1px 16px;height:100px;">
+    <h1>WELCOME TO MY WEBSITE</h1>
+
     <h2><br>
         <?php
         if((!empty($_GET['fname'])) && (!empty($_GET['lname']))) {
@@ -23,11 +32,13 @@
         }
         ?>
     </h2>
-    <p><a href="index.php?page=accounts&action=show">VIEW MY PROFILE</a><br></p>
-
-    <h2>TO-DO ITEMS</h2>
-    <a href = "index.php?page=tasks&action=create">CREATE TASK</a>
-    <a href="index.php?page=tasks&action=all">SHOW ALL TO-DO</a>
-
+<input style="width: 150px; font-family: Roboto, sans-serif; text-transform: uppercase; outline: 0; background: #4CAF50;  border: 0; padding: 15px;
+        color: #FFFFFF; font-size: 14px; cursor: pointer; position: relative" type="button" value="VIEW MY PROFILE" onclick="window.location.href='index.php?page=accounts&action=show'" />
+<h3>TO-DO ITEMS</h3>
+<input style="width: 150px; font-family: Roboto, sans-serif; text-transform: uppercase; outline: 0; background: #4CAF50;  border: 0; padding: 15px;
+        color: #FFFFFF; font-size: 14px; cursor: pointer; position: relative" type="button" value="CREATE TASK" onclick="window.location.href='index.php?page=tasks&action=create'" />
+<input style="width: 150px; font-family: Roboto, sans-serif; text-transform: uppercase; outline: 0; background: #4CAF50;  border: 0; padding: 15px;
+        color: #FFFFFF; font-size: 14px; cursor: pointer; position: relative" type="button" value="SHOW ALL TASKS" onclick="window.location.href='index.php?page=tasks&action=all'" />
+</div>
 </body>
 </html>
