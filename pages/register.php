9e4e66e36d51178595349cd9e7e50c38602676dd
Tanuka Nayak <tanuka1990@gmail.com>
2017-12-16 14:22:29 -0500

    Update register.php

--- pages/register.php
+++ pages/register.php
@@ -17,29 +17,26 @@
 
 <body>
 <form action="index.php?page=accounts&action=register" method="post">
-    <div class="login-page">
-        <div class="form">
-            <form class="register-form">
-                <input type="text" placeholder="First name" name="fname"><br>
-                <input type="text" placeholder="Last name" name="lname"><br>
-                <input type="text" placeholder="Email" name="email"><br>
-                <input type="text" placeholder="Phone" name="phone"><br>
-                <input type="date" placeholder="Birthday" name="birthday"><br>
-                <select name="gender">
-                    <option value="" disabled selected hidden>Gender</option>
-                    <option value="Female">Female</option>
-                    <option value="Male">Male</option>
-                </select>
-                <input type="password" placeholder="Password" name="password"><br>
-                <button type="submit" value="Submit form">Create</button>
-                <form action="index.php?page=accounts&action=login" method="get">
-                    <p class="message">Already registered? <a href="index.php?">Sign In</a></p>
-                </form>
+    <h1>REGISTRATION FORM</h1>
+    <div class="form" style="margin-bottom: 3%; width: 30%;">
+        <form class="register-form">
+            <input type="text" placeholder="First name" name="fname" required><br>
+            <input type="text" placeholder="Last name" name="lname" required><br>
+            <input type="email" placeholder="Email" name="email" required><br>
+            <input type="text" placeholder="Phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required title="Enter in the form of xxx-xxx-xxxx"><br>
+            <input type="text" placeholder="Birthday" name="birthday" onfocus="(this.type='date')" required title="Enter in the form of DD-MM-YYYY"><br>
+            <select name="gender">
+                <option value="" disabled selected hidden>Gender</option>
+                <option value="Female">Female</option>
+                <option value="Male">Male</option>
+            </select>
+            <input type="password" placeholder="Password" name="password" pattern=".{6,}" required title="Please enter at least 6 characters"><br>
+            <button type="submit" value="Submit form">Create</button>
+            <form action="index.php?page=accounts&action=login" method="get">
+                <p class="message">Already registered? <a href="index.php?">Sign In</a></p>
             </form>
-        </div>
+        </form>
     </div>
 </form>
-
-
 </body>
 </html>
